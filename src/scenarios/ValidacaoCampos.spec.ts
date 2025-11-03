732e22718f3778c835002c9afc4e4ed2fa1a68d4
gabrielfrello <gabrieltassifrello@hotmail.com>
2025-11-03 19:54:48 -0300

    commit4

--- src/scenarios/ValidacaoCampos.spec.ts
+++ src/scenarios/ValidacaoCampos.spec.ts
@@ -3,10 +3,22 @@ import { test, expect } from '@playwright/test';
 test('Validação de campos obrigatórios', async ({ page }) => {
   await page.goto('https://frellocalcados.com/contato');
 
+  await page.waitForSelector('#form-field-name', { state: 'visible' });
+  await page.waitForSelector('#form-field-email', { state: 'visible' });
+  await page.waitForSelector('#form-field-subject', { state: 'visible' });
+  await page.waitForSelector('#form-field-message', { state: 'visible' });
+
   await page.click('button[type="submit"]');
 
-  await expect(page.locator('#form-field-name')).toHaveAttribute('required', '');
-  await expect(page.locator('#form-field-email')).toHaveAttribute('required', '');
-  await expect(page.locator('#form-field-subject')).toHaveAttribute('required', '');
-  await expect(page.locator('#form-field-message')).toHaveAttribute('required', '');
+  await page.waitForTimeout(2000);
+
+  const nameValue = await page.locator('#form-field-name').inputValue();
+  const emailValue = await page.locator('#form-field-email').inputValue();
+  const subjectValue = await page.locator('#form-field-subject').inputValue();
+  const messageValue = await page.locator('#form-field-message').inputValue();
+
+  expect(nameValue).toBe('');
+  expect(emailValue).toBe('');
+  expect(subjectValue).toBe('');
+  expect(messageValue).toBe('');
 });
